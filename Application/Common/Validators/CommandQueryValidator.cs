5e356b30e32877664f48df4e6b4645e82a948b7b
Rafael <rafael_alejo@outlook.com>
2020-09-09 00:56:37 -0600

    Added common validator for job entity

---
+++ Application/Common/Validators/CommandQueryValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Validators
+{
+    public abstract class CommandQueryValidator<T> : AbstractValidator<T>
+    {
+        protected IApplicationDbContext Context { get; private set; }
+
+        protected CommandQueryValidator(IApplicationDbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        protected async Task<bool> JobExists(int id, CancellationToken cancellationToken)
+        {
+            return await Context.Jobs.AnyAsync(j => j.Id == id, cancellationToken);
+        }
+    }
+}
