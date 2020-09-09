3d909e3d728f2e63e9fa6adb0110fcec56133599
Rafael <rafael_alejo@outlook.com>
2020-09-08 23:46:09 -0600

    Added Job entity db configuration

---
+++ Persistence/Configurations/JobConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    public class JobConfiguration : IEntityTypeConfiguration<Job>
+    {
+        public void Configure(EntityTypeBuilder<Job> builder)
+        {
+            // Primary key
+            builder.HasKey(j => j.Id);
+            builder.Property(j => j.Id)
+                .ValueGeneratedOnAdd();
+
+            // Properties
+            builder.Property(j => j.Title)
+                .IsRequired();
+            builder.Property(j => j.Description)
+                .IsRequired();
+        }
+    }
+}
