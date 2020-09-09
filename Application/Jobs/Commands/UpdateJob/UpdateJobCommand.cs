f0d6aaac37c931f7bbfa5056b77ab5b6766d20d2
Rafael <rafael_alejo@outlook.com>
2020-09-09 00:56:59 -0600

    Added command for updating a job entity

---
+++ Application/Jobs/Commands/UpdateJob/UpdateJobCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Jobs.Commands.UpdateJob
+{
+    public class UpdateJobCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? ExpiresAt { get; set; }
+    }
+
+    public class UpdateJobCommandHandler : IRequestHandler<UpdateJobCommand>
+    {
+        private readonly IApplicationDbContext context;
+
+        public UpdateJobCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Unit> Handle(UpdateJobCommand request, CancellationToken cancellationToken)
+        {
+            var job = await context.Jobs.FindAsync(request.Id);
+
+            job.Title = request.Title ?? job.Title;
+            job.Description = request.Description ?? job.Description;
+            job.ExpiresAt = request.ExpiresAt ?? job.ExpiresAt;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Application/Jobs/Commands/UpdateJob/UpdateJobCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Validators;
+using FluentValidation;
+using System;
+
+namespace Application.Jobs.Commands.UpdateJob
+{
+    public class UpdateJobCommandValidator : CommandQueryValidator<UpdateJobCommand>
+    {
+        public UpdateJobCommandValidator(IApplicationDbContext context) : base(context)
+        {
+            RuleFor(c => c.Id)
+                .MustAsync(JobExists)
+                .WithMessage("Invalid job id.");
+
+            RuleFor(c => c.Title)
+                .NotEmpty().When(c => c.Title != null);
+            RuleFor(c => c.Description)
+                .NotEmpty().When(c => c.Description != null);
+            RuleFor(c => c.ExpiresAt)
+                .GreaterThan(DateTime.Now).When(c => c.ExpiresAt.HasValue)
+                .WithMessage("Expiration date should be in the future.");
+        }
+    }
+}
