54bb8be08a6af2b6226988f0488c0a8e8a85272f
Rafael <rafael_alejo@outlook.com>
2020-09-08 23:27:49 -0600

    Added persistence layer interface

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+
+    public interface IApplicationDbContext
+    {
+        DbSet<Job> Jobs { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
