5d906cfe4049395768b82f384041f9698060fd14
Rafael Pérez <rafael_alejo@outlook.com>
2020-09-08 22:44:18 -0600

    Started Application layer with MediatR pipeline and dependency injection extension

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+using Application.Common.Behaviours;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+
+            return services;
+        }
+    }
+}
