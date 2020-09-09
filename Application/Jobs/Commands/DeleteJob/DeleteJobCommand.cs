0eaefd5e46df222c71a63ef585dff579e4bfb5d6
Rafael <rafael_alejo@outlook.com>
2020-09-09 00:57:18 -0600

    Added command for removing a job from the database

---
+++ Application/Jobs/Commands/DeleteJob/DeleteJobCommand.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Jobs.Commands.DeleteJob
+{
+    public class DeleteJobCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeleteJobCommandHandler : IRequestHandler<DeleteJobCommand>
+    {
+        private readonly IApplicationDbContext context;
+
+        public DeleteJobCommandHandler(IApplicationDbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Unit> Handle(DeleteJobCommand request, CancellationToken cancellationToken)
+        {
+            var job = await context.Jobs.FindAsync(request.Id);
+
+            context.Jobs.Remove(job);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Application/Jobs/Commands/DeleteJob/DeleteJobCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Validators;
+using FluentValidation;
+
+namespace Application.Jobs.Commands.DeleteJob
+{
+    public class DeleteJobCommandValidator : CommandQueryValidator<DeleteJobCommand>
+    {
+        public DeleteJobCommandValidator(IApplicationDbContext context) : base(context)
+        {
+            RuleFor(c => c.Id)
+                .MustAsync(JobExists)
+                .WithMessage("Invalid job id.");
+        }
+    }
+}
