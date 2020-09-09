a4f5bacbe1f4565c966c00497e4f6ffa5cc78c8a
Rafael Pérez <rafael_alejo@outlook.com>
2020-09-08 22:23:49 -0600

    Added Domain layer with Job entity

---
+++ Domain/Entities/Job.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Job
+    {
+        public Job(string title, string description, DateTime createdAt, DateTime expiresAt)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            CreatedAt = createdAt;
+            ExpiresAt = expiresAt;
+        }
+
+        public int Id { get; private set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+    }
+}
