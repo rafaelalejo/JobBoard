baeb32c9f35f44bb58fa378c39d7244201706ce2
Rafael <rafael_alejo@outlook.com>
2020-09-08 23:34:28 -0600

    Added Persistence layer definition

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
