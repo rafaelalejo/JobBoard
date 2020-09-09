dcb1ed07490a71b289ac02428c48bbf223c3e324
Rafael <rafael_alejo@outlook.com>
2020-09-09 00:57:42 -0600

    Implemented all commands in job controller

--- WebAPI/Controllers/JobsController.cs
+++ WebAPI/Controllers/JobsController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Jobs.Commands.CreateJob;
+using Application.Jobs.Commands.DeleteJob;
+using Application.Jobs.Commands.UpdateJob;
 using Application.Jobs.Queries.Common;
 using Application.Jobs.Queries.GetAllJobs;
 using Microsoft.AspNetCore.Mvc;
@@ -15,9 +17,25 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<int>> Post(CreateJobCommand command)
+        public async Task<ActionResult<int>> Create(CreateJobCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult> Update(UpdateJobCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeleteJobCommand { Id = id, });
+
+            return NoContent();
         }
     }
 }
